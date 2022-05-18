6cc29bcd95acdea40af625809e3c6ecbe4249987
inerpiece <anton.p.markov@gmail.com>
2022-05-18 19:19:07 +0200

    Now can fetch cars, and singleCar through the /car/:slug route. All props are accessible and being rendered. Next up is styling.

--- frontend/src/components/car-components/SingleCar.js
+++ frontend/src/components/car-components/SingleCar.js
@@ -1,8 +1,55 @@
 import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 import sanityClient from "../../client";
+import Loading from '../loading';
 
 export default function SingleCar() {
+  const [singleCarData, setSingleCarData] = useState(null);
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(`*[slug.current == "${slug}"]{
+        name,
+        "brand": brand[]->name,
+        fuelType,
+        fuelConsumption,
+        transmition,
+        price,
+        seats,
+        "category": category[]->category,
+        image{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`)
+      .then((data) => setSingleCarData(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!singleCarData) return <Loading />;
+
   return (
-    <h1>SingleCar page!</h1>
+    <main>
+      <article>
+        <header>
+          <div>
+            <div>
+              <h1>{singleCarData.name}</h1>
+              <div>
+                <img src={singleCarData.image.asset.url} alt={singleCarData.name} />
+              </div>
+              <p>{singleCarData.category}</p>
+            </div>
+          </div>
+          <img />
+        </header>
+        <div>blc</div>
+      </article>
+    </main>
   )
 }
