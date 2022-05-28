644ca1a0474362fa7d968e9aa1b5ee0245a6b70a
inerpiece <anton.p.markov@gmail.com>
2022-05-28 14:47:49 +0200

    fixed the path inside of the Banner.test.js relating to the banner component

--- frontend/src/components/Banner/_test_/Banner.test.js
+++ frontend/src/components/Banner/_test_/Banner.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import Banner from '../../Banner';
+import { render } from '@testing-library/react';
+import Banner from '../../ui/Banner';
 import '@testing-library/jest-dom';
 
 describe("Banner", () => {
